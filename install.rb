78a7beae42319d0763e072e7169c92c77401c5bd
Sean Marman <smarman@EMVMBPSMARMAN.local>
2015-08-01 21:31:45 -0700

    Changed the mustang color scheme installer.

--- install.rb
+++ install.rb
@@ -3,8 +3,14 @@ puts "############### Installing Vundle  ####################"
 puts "############### and my preferences ####################"
 puts "#######################################################"
 
+puts "############ Install Vundle Package Manager ###########"
 `git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim`
+puts "############ Make and Copy Vimrc pref #################"
 `touch ~/.vimrc && cat vimrc.txt >> ~/.vimrc`
-`cd ~/.vim/colors && git clone https://github.com/croaker/mustang-vim.git`
+puts "############ Add Mustange Color Scheme ################"
+`mkdir -p ~/.vim/colors && cat mustang.txt > ~/.vim/colors/mustang.vim`
+#`cd ~/.vim/colors && git clone https://github.com/croaker/mustang-vim.git`
+puts "############ Run the pulgin Installer #################"
 `vim +PluginInstall +qall`
+puts "############ Append to the bash_profile ###############"
 `cat bash_profile.txt >> ~/.bash_profile`
